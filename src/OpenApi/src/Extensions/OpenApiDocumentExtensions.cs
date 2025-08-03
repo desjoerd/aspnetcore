4da2fb57bfa36af362ae416435dba9e710181f9c
Sjoerd van der Meer <sjoerd.vandermeer@xebia.com>
2025-08-03 14:07:48 +0200

    Openapi: Add metadata properties to be able to modify the openapi reference

--- src/OpenApi/src/Extensions/OpenApiDocumentExtensions.cs
+++ src/OpenApi/src/Extensions/OpenApiDocumentExtensions.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Text.Json.Nodes;
+
 namespace Microsoft.AspNetCore.OpenApi;
 
 internal static class OpenApiDocumentExtensions
@@ -21,6 +23,16 @@ internal static class OpenApiDocumentExtensions
         document.Workspace ??= new();
         var location = document.BaseUri + "/components/schemas/" + schemaId;
         document.Workspace.RegisterComponentForDocument(document, schema, location);
-        return new OpenApiSchemaReference(schemaId, document);
+
+        object? description = null;
+        object? example = null;
+        ((OpenApiSchema)schema).Metadata.TryGetValue("x-ref-description", out description);
+        ((OpenApiSchema)schema).Metadata.TryGetValue("x-ref-example", out example);
+
+        return new OpenApiSchemaReference(schemaId, document)
+        {
+            Description = description as string,
+            Examples = example is JsonNode exampleJsonNode ? [exampleJsonNode] : null,
+        };
     }
 }
